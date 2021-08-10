c3e0de5765b5dadaa1747e6e906eda3e455cea98
crossddelta <danilo.lessa@aluno.ifsp.edu.br>
2021-08-09 21:23:54 -0300

    Exercícios do curso de Python da Geek University

---
+++ secao03/exercicio4.py
@@ -0,0 +1,6 @@
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("Informe o segundo numero: "))
+
+soma = num1 + num2
+
+print("A soma dos dois valores é: {0}".format(soma))
---
+++ secao03/exercicio5.py
@@ -0,0 +1,5 @@
+metros = int(input("Informe o valor em metros: "))
+
+cent = metros * 100
+
+print("O valor de {0} metros convertido em centímetros é igual a: {1}cm".format(metros, cent))
